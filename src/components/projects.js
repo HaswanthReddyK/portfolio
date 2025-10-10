a6e97f61fcb91ed7529d658e9fe52a8e8da1b3c4
Haswanth Reddy Kondamadugula <hrkondamadugula13@gmail.com>
2025-10-10 16:19:45 -0400

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,7 +16,7 @@ class Projects extends Component{
                                 <NumpyOriginal size={iconSize}/>
                             </div>,
                 description: "In this project, I developed a neural network model inspired by Marc Tanti's merge model, designed to generate descriptive captions for any input image. This system integrates two distinct models: a word embedding model for text processing and a CNN for image analysis. These components converge in a LSTM network to produce image captions.",
-                link: "https://github.com/Yogesh-sarma/ImageCaptionGenerator"
+                link: "https://github.com/HaswanthReddyK/Image-Caption-Generator"
             },
             {
                 title: "PPM Tool",
@@ -28,7 +28,7 @@ class Projects extends Component{
                                 <ReactbootstrapOriginal size={iconSize}/>
                             </div>,
                 description: "A project management tool inspired by the Kanban methodology, designed to track and manage project tasks. This tool features unique project identifiers, enabling precise task organization. Users can assign priorities and statuses to tasks, as well as add or delete them. It also includes authentication functionalities, ensuring secure access and personalized user experiences.",
-                link: "https://github.com/Yogesh-sarma/Project-Management-Tool"
+                link: "https://github.com/HaswanthReddyK/Project-Management-Tool"
             },
             {
                 title: "Facial Expression Recognition",
@@ -40,7 +40,7 @@ class Projects extends Component{
                                 <MatplotlibOriginal size={iconSize} title="Matplotlib"/>
                             </div>,
                 description: "Developed a facial expression recognition model capable of detecting six distinct emotions. Leveraging HAAR Cascade filters for precise face detection and training the model with the Cohn-Kanade dataset, I achieved robust performance. The model utilizes the FisherFace classifier for facial expression classification. It also features real-time emotion detection capabilities through a webcam interface.",
-                link: "https://github.com/Yogesh-sarma/Facial-Expression-recognition"
+                link: "https://github.com/HaswanthReddyK/Facial-Expression-recognition"
             },
             {
                 title: "My Portfolio Website",
@@ -50,7 +50,7 @@ class Projects extends Component{
                                 <JavascriptOriginal size={iconSize}/>
                             </div>,
                 description: "Yes, You are currently in it!",
-                link: "https://github.com/Yogesh-sarma/portfolio"
+                link: "https://github.com/HaswanthReddyK/portfolio"
             },
             {
                 title: "Weather Data Analysis using Hadoop and Hive",
@@ -60,7 +60,7 @@ class Projects extends Component{
                                 <SiApachehive size={iconSize}/>
                             </div>,
                 description: "I developed a climate data analysis system to process weather data of the past 25 years from the GHCNd. The system ensures data quality through consistency checks and outlier detection. It includes MapReduce jobs for temperature and precipitation analysis, providing average temperatures and total precipitation over specified periods. HiveQL queries identify extreme weather events and analyze long-term climate trends, offering insights into regional variations and supporting informed decision-making on climate change.",
-                link: "https://github.com/Yogesh-sarma/WeatherAnalysis"
+                link: "https://github.com/HaswanthReddyK/Weather-Analysis"
             },
             {
                 title: "Fraud Detection using Spark",
@@ -69,7 +69,7 @@ class Projects extends Component{
                                 <ApachesparkOriginal size={iconSize}/>
                             </div>,
                 description: "Developed a fraud detection system for analyzing large datasets in real time to detect potential fraud. It includes data preprocessing, EDA, and feature engineering with Spark MLlib. I implemented ML models for fraud detection, evaluating performance with different metrics like F-measure. The scalable pipeline supports real-time processing via Structured Streaming. Standalone Spark applications provide user behavior analysis, transaction frequency analysis, merchant analysis, and geospatial analysis, offering comprehensive insights into fraud patterns.",
-                link: "https://github.com/Yogesh-sarma/Fraud-detection-spark"
+                link: "https://github.com/HaswanthReddyK/Fraud-detection-spark"
 
             },
             {
@@ -80,7 +80,7 @@ class Projects extends Component{
                                 <Html5Original size={iconSize}/>
                             </div>,
                 description: "Designed and implemented an interactive scatterplot visualization using D3.js, capable of handling and displaying temporal CSV data. This dynamic tool features a time selection slider for intuitive data exploration and offers informative tooltips for each data point, revealing additional attributes from the dataset. To enhance visual distinction, I applied a diverging colormap, ensuring clear and insightful data representation.",
-                link: "https://github.com/Yogesh-sarma/ConnectedScatterplotVisualisation"
+                link: "https://github.com/HaswanthReddyK/Connected-Scatterplot-Visualisation"
             }            
         ]
 
